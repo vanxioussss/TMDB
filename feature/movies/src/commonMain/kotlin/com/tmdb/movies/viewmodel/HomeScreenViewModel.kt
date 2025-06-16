5c76c6a2b98e73d09ae992103041f6bbd31e2b42
vanxioussss <vanluong.vnln@gmail.com>
2025-06-17 03:29:56 +0700

    Implement search feature and switch between search and trending

--- feature/movies/src/commonMain/kotlin/com/tmdb/movies/viewmodel/HomeScreenViewModel.kt
+++ feature/movies/src/commonMain/kotlin/com/tmdb/movies/viewmodel/HomeScreenViewModel.kt
@@ -5,8 +5,19 @@ import androidx.lifecycle.viewModelScope
 import com.tmdb.common.resource.Resource
 import com.tmdb.domain.usecases.GetTrendingMoviesUseCase
 import com.tmdb.domain.usecases.SearchMoviesUseCase
+import com.tmdb.model.Movie
 import com.tmdb.movies.state.MovieUiState
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 
@@ -19,24 +30,46 @@ class HomeScreenViewModel(
     private val searchMoviesUseCase: SearchMoviesUseCase
 ) : ViewModel() {
 
-    val movieUiState = trendingMoviesUseCase()
-        .map { resource ->
-            when (resource) {
-                is Resource.Success -> MovieUiState.Success(resource.body)
-                is Resource.DataError -> MovieUiState.Error(
-                    resource.error.message ?: "Unknown error"
-                )
+    private val _searchQuery = MutableStateFlow("")
+    val searchQuery: StateFlow<String> = _searchQuery.asStateFlow()
 
-                is Resource.ServerError -> MovieUiState.Error(
-                    resource.error.message
-                )
+    private val trendingMovies: StateFlow<Resource<List<Movie>>> = trendingMoviesUseCase()
+        .stateIn(viewModelScope, SharingStarted.Lazily, Resource.Loading)
 
-                is Resource.Loading -> MovieUiState.Loading
+    /**
+     * Represents the UI state for the movie list screen.
+     *
+     * This state is updated based on the search query or trending movies.
+     */
+    @OptIn(ExperimentalCoroutinesApi::class, FlowPreview::class)
+    val movieUiState: StateFlow<MovieUiState> = _searchQuery
+        .debounce(500)
+        .distinctUntilChanged()
+        .flatMapLatest { query ->
+            if (query.isNotBlank()) {
+                flowOf(MovieUiState.ShowSearch(searchMoviesUseCase(query)))
+            } else {
+                trendingMovies.map { resource ->
+                    when (resource) {
+                        is Resource.Success -> MovieUiState.ShowTrending(resource.body)
+                        is Resource.DataError -> MovieUiState.Error(
+                            resource.error.message ?: "Unknown error"
+                        )
+
+                        is Resource.ServerError -> MovieUiState.Error(resource.error.message)
+                        is Resource.Loading -> MovieUiState.Loading
+                    }
+                }
             }
         }
+        .catch { MovieUiState.Error(it.message ?: "Unknown error") }
         .stateIn(
-            scope = viewModelScope,
-            started = SharingStarted.Lazily,
-            initialValue = MovieUiState.Loading
+            viewModelScope,
+            SharingStarted.Lazily,
+            MovieUiState.Loading
         )
+
+    fun updateSearchQuery(query: String) {
+        _searchQuery.value = query
+    }
 }
